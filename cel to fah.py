807d79d9e313c25efab7c9f5005f7acee008518b
Thechromebookguy <75165778+Thechromebookguy@users.noreply.github.com>
2021-03-16 13:40:32 +0000

    Add files via upload

---
+++ cel to fah.py
@@ -0,0 +1,12 @@
+# Simple program to convert delsius fahrenheit
+
+
+
+
+cel = int(input('Enter the celsius value: '))
+
+# calculate celsius value
+fah = (cel * 9/5) + 32
+
+# display celsius value
+print(cel, "degrees c is", fah, "degrees f")
---
+++ fah_to_cel.py
@@ -0,0 +1,8 @@
+# Simple program to convert fahrenheit to celsius
+fah = int(input('Enter the fahrenheit value: '))
+
+# calculate celsius value
+cel = (fah - 32) * 5 / 9
+
+# display celsius value
+print(fah, "degrees F is", cel, "degrees C")
